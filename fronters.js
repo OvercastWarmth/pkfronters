89c202ec39d0fa937e5ee6c3fd5252be9b4b2806
Ringlings <110815694+Ringlings@users.noreply.github.com>
2022-09-04 20:55:51 +1000

    Promt inputting system ID

--- fronters.js
+++ fronters.js
@@ -1,13 +1,21 @@
 const queryString = window.location.search;
 const system = new URLSearchParams(queryString).get("sys");
+const container = document.querySelector('.container');
 
 async function getFronters() {
     let response = await fetch("https://api.pluralkit.me/v2/systems/" + system + "/fronters");
+    if (response.status != 200) {
+        showInput(response.status)
+        return null
+    }
     return await response.json()
 }
 
 async function renderFronters() {
-    let fronters = await getFronters();
+    const fronters = await getFronters();
+    if (fronters == null) {
+        return
+    }
     let html = '';
     fronters.members.forEach(fronter => {
         let avatar
@@ -32,5 +40,29 @@ async function renderFronters() {
     container.innerHTML = html;
 }
 
-renderFronters();
+function showInput(reason) {
+    console.log(reason)
+    let label;
+    if (reason == 404) {
+        label = "There is no system by that ID."
+    }
+    else if (reason == 403) {
+        label = "This system has their fronters hidden."
+    }
+    else if (reason == null) {
+        label = "Please enter a system ID:"
+    };
+    container.innerHTML = `<form>
+                            <label name="sys">${label}</label>
+                            <input type="text" name="sys">
+                            <input type="submit">
+                        </form>`
+}
 
+if (system != null & system != "") {
+    container.innerHTML = `<code>Loading fronters...</code>`
+    renderFronters();
+}
+else {
+    showInput(null)
+};
